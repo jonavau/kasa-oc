98242e8aa00a58eb318fbee49ec2f684fa9afac3
greskelll <jonattan44@hotmail.fr>
2023-03-03 16:30:45 +0100

    Initialisation du Projet Kasa Suppression fichier superflu Création sous-dossier components et styles

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>hello world</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
