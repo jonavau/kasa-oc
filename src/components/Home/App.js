218fee3abb1dce018ddc6844842507a227e9d5f1
greskelll <jonattan44@hotmail.fr>
2023-03-08 17:42:08 +0100

    Création composant Footer Intégration Footer à l'App Début CSS Footer

--- src/components/Home/App.js
+++ src/components/Home/App.js
@@ -2,31 +2,31 @@ import Banner from '../Banner';
 import Header from '../Header';
 import Gallery from '../Gallery';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFound from '../NotFound';
 import AboutUs from '../About_us';
+import Footer from '../Footer';
 
 function App() {
 	return (
-		<React.Fragment>
-			<Router>
-				<Header />
-				<Routes>
-					<Route
-						exact
-						path="/"
-						element={
-							<React.Fragment>
-								<Banner />
-								<Gallery />
-							</React.Fragment>
-						}
-					/>
-					<Route exact path="/404" element={<NotFound />} />
-					<Route exact path="/about_us" element={<AboutUs />} />
-				</Routes>
-			</Router>
-		</React.Fragment>
+		<Router>
+			<Header />
+			<Routes>
+				<Route
+					exact
+					path="/"
+					element={
+						<React.Fragment>
+							<Banner />
+							<Gallery />
+						</React.Fragment>
+					}
+				/>
+				<Route path="/about_us" element={<AboutUs />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+			<Footer />
+		</Router>
 	);
 }
 
