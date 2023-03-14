9aa200217f1075ee17a9f14927a3432c0116f9d7
greskelll <jonattan44@hotmail.fr>
2023-03-14 16:52:05 +0100

    Création Carroussel avec useState clic gauche et droite OK à Rajouter Carroussel infini

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,17 +1,31 @@
 import { useState } from 'react';
+import leftArrow from '../assets/leftArrow.png';
+import rightArrow from '../assets/rightArrow.png';
+import '../styles/Carousel.css';
 
 function Carousel({ liste }) {
+	const [index, updateIndex] = useState(0);
+	console.log(index);
 	return (
-		<div className="Carousel">
-			{liste.pictures.map((pic) => (
-				<img className="active" alt={liste.description} src={pic}></img>
-			))}
+		<div className="carousel">
+			{/* {liste.pictures.map((pic) => (
+				<img alt={liste.description} src={pic}></img>
+			))} */}
+			<img alt={liste.description} src={liste.pictures[index]}></img>
+			<img
+				onClick={() => updateIndex(index - 1)}
+				src={leftArrow}
+				alt="flèche gauche"
+				className="leftArrow"
+			></img>
+			<img
+				onClick={() => updateIndex(index + 1)}
+				src={rightArrow}
+				alt="flèche droite"
+				className="rightArrow"
+			></img>
 		</div>
 	);
-}
-
-function Switch() {
-	const [pic, updatePic] = useState(0);
 }
 
 export default Carousel;
