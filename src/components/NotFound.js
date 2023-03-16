6b10713b49102b919e7af9aa342925969981215b
greskelll <jonattan44@hotmail.fr>
2023-03-16 14:04:40 +0100

    Réalisation page 404 conforme a maquette

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,14 @@
 import '../styles/NotFound.css';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
 	return (
 		<div className="NotFound">
-			<h1>404 Not fOUND</h1>
+			<h1>404</h1>
+			<h2>Oups! La page que vous demandez n'existe pas.</h2>
+			<Link className="Link" to="/">
+				Retourner sur la page d’accueil
+			</Link>
 		</div>
 	);
 }
