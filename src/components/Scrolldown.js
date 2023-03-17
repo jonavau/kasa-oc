9d434b74eacf3b68ab48d85362c942f86031a3de
greskelll <jonattan44@hotmail.fr>
2023-03-17 11:43:13 +0100

    rajout description fonction scrolldown

--- src/components/Scrolldown.js
+++ src/components/Scrolldown.js
@@ -2,6 +2,10 @@ import vector from '../assets/Vector.png';
 import '../styles/ScrollDown.css';
 import { useState } from 'react';
 
+/*le composant ScrollDown utilise un state fermé par défaut, au clic sur la flèche le
+state change et contenu de la div change et affiche le texte en dessous, on peut ensuite
+recliquer sur la flèche pour collapse */
+
 function Scrolldown({ title, text }) {
 	const [isClosed, setIsClosed] = useState(true);
 	return isClosed ? (
